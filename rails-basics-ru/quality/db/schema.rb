f5b0fb53e6c86f980d5a76f200b654362209edcb
@hexlet/cli <support@hexlet.io>
2022-10-23 10:29:59 +0500

    submit rails-basics-ru/quality

--- rails-basics-ru/quality/db/schema.rb
+++ rails-basics-ru/quality/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_131119) do
+ActiveRecord::Schema.define(version: 2022_09_22_092447) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string "title"
